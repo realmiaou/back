d7dc40c599ea6b8c67ed5fa4026ea24bc57e629c
Thibaut Mottet <thibaut.mottet@gmail.com>
2023-03-14 11:40:37 +0400

    fix(mail): add text

--- src/mail/index.ts
+++ src/mail/index.ts
@@ -4,36 +4,50 @@ import fg from 'fast-glob'
 import mjml2html from 'mjml'
 import yaml from 'yaml'
 import handlebars from 'handlebars'
+import { LanguageIso } from '@miaou/types'
 import { GeneratedMail, MailToGenerate } from './index.type'
 
 export const generateMail = async ({ templateFileName, locale = 'en', variables, srcPath }: MailToGenerate) => {
-  // i18nToCompile
-  const [i18nPath] = await fg(
-    path.join(srcPath, `**/${templateFileName}.i18n.yml`)
-  )
+  const [i18nPath] = await fg(path.join(srcPath, `**/${templateFileName}.i18n.yml`))
+  initI18n(i18nPath, locale, variables)
+
+  const subject = toSubject(i18nPath, locale, variables)
+
+  const [mjmlPath] = await fg(path.join(srcPath, `**/${templateFileName}.mjml`))
+  const html = toMjml(mjmlPath, variables)
+
+  const [textPath] = await fg(path.join(srcPath, `**/${templateFileName}.txt`))
+  const text = toText(textPath, variables)
+
+  return { html: html ?? text ?? '', subject: subject ?? '', text: text ?? '' } as GeneratedMail
+}
+
+const initI18n = (i18nPath: string | undefined, locale: LanguageIso, variables = {}) => {
+  if (!i18nPath) return
   const i18nToCompile = fs.readFileSync(i18nPath).toString()
   const templateI18n = yaml.parse(i18nToCompile)
   handlebars.registerHelper('$t', key =>
     handlebars.compile(templateI18n[locale][key] ?? '')(variables)
   )
-  const subject = handlebars.compile(templateI18n[locale].subject ?? '')(
-    variables
-  )
+}
 
-  // mjml
-  const [templatePath] = await fg(
-    path.join(srcPath, `**/${templateFileName}.mjml`)
-  )
-  const templateToCompile = fs.readFileSync(templatePath).toString()
-  const compiledTemplate = handlebars.compile(templateToCompile)(variables)
-  const { html } = mjml2html(compiledTemplate)
+const toSubject = (i18nPath: string | undefined, locale: LanguageIso, variables = {}) => {
+  if (!i18nPath) return null
+  const i18nToCompile = fs.readFileSync(i18nPath).toString()
+  const templateI18n = yaml.parse(i18nToCompile)
+  return handlebars.compile(templateI18n[locale].subject ?? '')(variables)
+}
 
-  // text
-  const [textPath] = await fg(
-    path.join(srcPath, `**/${templateFileName}.txt`)
-  )
+const toMjml = (mjmlPath: string | undefined, variables = {}) => {
+  if (!mjmlPath) return null
+  const mjmlTemplate = fs.readFileSync(mjmlPath).toString()
+  const mjmlCompiled = handlebars.compile(mjmlTemplate)(variables)
+  const { html } = mjml2html(mjmlCompiled)
+  return html
+}
+
+const toText = (textPath: string | undefined, variables = {}) => {
+  if (!textPath) return null
   const textToCompile = fs.readFileSync(textPath).toString()
-  const text = handlebars.compile(textToCompile)(variables)
-
-  return { html, subject, text } as GeneratedMail
+  return handlebars.compile(textToCompile)(variables)
 }
